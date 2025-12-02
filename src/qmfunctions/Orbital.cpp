6afab1bce86f7fc51ef7660f38c92606836b63f8
Quentin Pitteloud <quentin.pitteloud@uit.no>
2025-12-02 13:01:23 +0100

    tut

--- src/qmfunctions/Orbital.cpp
+++ src/qmfunctions/Orbital.cpp
@@ -43,13 +43,13 @@ Orbital::Orbital()
  *
  * Initializes with NULL tree pointers.
  */
-Orbital::Orbital(SPIN::type spin)
-        : mrcpp::CompFunction<3>(spin) {
+Orbital::Orbital(SPIN::type spin, n_component)
+        : mrcpp::CompFunction<3>(spin, n_component) {
     if (this->spin() < 0) INVALID_ARG_ABORT;
     // d1 is used to store occupancy
     if (this->spin() == SPIN::Paired) this->func_ptr->data.d1[0] = 2;
     if (this->spin() == SPIN::Alpha) this->func_ptr->data.d1[0] = 1;
-    if (this->spin() == SPIN::Beta) this->func_ptr->data.d1[0] = 1;
+    if (this->spin() == SPIN::Beta) this->func_ptr->data.d1[1] = 1;
 }
 
 /** @brief Constructor
